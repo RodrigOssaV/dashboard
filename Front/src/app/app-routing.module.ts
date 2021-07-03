0902ecf1b7d3b146f5a6c8fa79ed9ac81ea934ef
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-07-02 23:07:13 -0400

    front driver
    driver html css ts

--- Front/src/app/app-routing.module.ts
+++ Front/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { DriverComponent } from "./pages/driver/driver.component";
+
+const routes: Routes = [
+  { path: '', redirectTo: 'Dashboard', pathMatch:'full' },
+  { path: 'Dashboard', component: DashboardComponent },
+  { path: 'Driver', component: DriverComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
