62ca8ac0e28ba363255c199d8df671180330db06
RodrigOssaV <49349061+RodrigOssaV@users.noreply.github.com>
2021-07-10 12:21:38 -0400

    form add supplier
    form add supplier incorporate and working

--- Front/src/app/pages/supplier/supplier-components/add-form/add-form.component.ts
+++ Front/src/app/pages/supplier/supplier-components/add-form/add-form.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Supplier } from "../../../../class/supplier";
+import { SupplierService } from "../../../../service/supplier/supplier.service";
+import { NotificationService } from "../../../../service/component/notification.service";
+
 @Component({
   selector: 'app-add-form',
   templateUrl: './add-form.component.html',
@@ -7,9 +11,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddFormComponent implements OnInit {
 
-  constructor() { }
+  newSupplier: Supplier = new Supplier();
+
+  constructor(
+    private apiSupplier: SupplierService,
+    private notifyService: NotificationService) { }
 
   ngOnInit(): void {
+  }
+
+  addSupplier(form:any){
+    this.apiSupplier.add_supplier(form.value).subscribe(
+      res => {
+        this.notifyService.showSuccess("Success incorporate supplier","Notificación");
+        form.reset();
+        this.launchModal();
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
 
   launchModal(){
